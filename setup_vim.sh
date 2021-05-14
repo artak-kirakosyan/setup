83ee2a6902e1547e844fd705e86aa0ffe550a1b1
Artak <artakkirakosyan96@mail.ru>
2021-05-14 12:15:03 +0400

    Make vim default for git

--- setup_vim.sh
+++ setup_vim.sh
@@ -21,3 +21,4 @@ echo "alias l='ls -CF'" >> ~/.bashrc
 
 # this one is not a vim related, but is very useful
 git config --global alias.lga "log --graph --oneline --all --decorate"
+git config --global core.editor "vim"
